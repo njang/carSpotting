6913ffd39ab9ba56666e83c341dbd6642e852500
njang <neo.w.jang@gmail.com>
2018-01-17 11:58:19 -0600

    Initiate express app & index.html

--- app.js
+++ app.js
@@ -25,6 +25,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+/**********
+ * SERVER *
+ **********/
+
+// listen on port 3000
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is running on http://localhost:3000/');
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+/**********
+ * ROUTES *
+ **********/
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function homepage (req, res, next) {
+  res.render('index', { title: 'Lawn Tracker' });
+});
+
+/* JSON API Endpoints */
+
+router.get('/api', function api_index (req, res){
+  res.json({
+    message: "Lawn Tracker App for Lawnsworth",
+    documentation_url: "https://github.com/njang/lawnTracker/blob/master/README.md",
+    base_url: "",
+    endpoints: [
+      {method: "GET", path: "/api", description: "Describes available endpoints"}
+    ]
+  });
 });
 
 module.exports = router;
