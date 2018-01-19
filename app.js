f31ff1d36e93d5c2b60fbcc520d7ae3ee550e39b
njang <neo.w.jang@gmail.com>
2018-01-18 19:45:31 -0600

    WIP debug Oauth

--- app.js
+++ app.js
@@ -48,12 +48,11 @@ passport.use(new GoogleStrategy({
     callbackURL: "http://127.0.0.1:3000/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
-		User.findOrCreate({ googleId: profile.id }, function (err, user) {
+		User.findOrCreate({ 'google.id': profile.id }, function (err, user) {
 			return done(err, user);
 		});
   }
 ));
-
 
 /**********
  * SERVER *
