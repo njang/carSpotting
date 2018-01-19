7b41eb4b37b1e6ce2a94bc2c4ad8b48bfa3fc63e
njang <neo.w.jang@gmail.com>
2018-01-18 21:32:50 -0600

    Populate test entries to seed database

--- models/client.js
+++ models/client.js
@@ -4,11 +4,19 @@ const Schema = mongoose.Schema;
 
 const ClientSchema = new Schema({
   name: String,
-  address: String,
+  location: {
+    streetAddress: String,
+    coordinates: {
+      lat: Number,
+      lng: Number
+    }
+  },
   phone: Number,
-  lotSize: Number,
-  turfType: String,
-  lastMowed: Number
+  lawn: {
+    lotSize: Number,
+    turfType: String,
+    lastMowed: Number
+  }
 });
 
 const Client = mongoose.model('Client', ClientSchema);
--- seed.js
+++ seed.js
@@ -1,33 +1,123 @@
 // This file allows us to seed our application with data
-// simply run: `node seed.js` from the root of this project folder.
+// Run `node seed.js` from the root of this project folder.
 
-//require ./models S1S5 TC
 var db = require("./models");
 
 var clientList =[];
-//hard-coded model data S1S4 TC
 clientList.push({
-  name: 'Jason Bourne',
-  address: String,
-  phone: Number,
-  lotSize: Number,
-  turfType: String,
-  lastMowed: Number
+  name: "Samantha Duncan",
+  phone: 9793825018,
+  location: { 
+    streetAddress: "2908 Bolero Ct. College Station, TX 77845", 
+    coordinates: { lat: 30.580330, lng: -96.313033 } 
+  },
+  lawn: { lotSize: 0.25, turfType: "St. Augustine", lastMowed: 1501385439 }
+});
+clientList.push({
+  name: "Jonathan Ransom",
+  phone: 9795731839,
+  location: { 
+    streetAddress: "2905 Aztec Ct. College Station, TX 77845", 
+    coordinates: { lat: 30.581416, lng: -96.312349 } 
+  },
+  lawn: { lotSize: 0.24, turfType: "St. Augustine", lastMowed: 1501385439 }
+});
+clientList.push({
+  name: "Meghan Allen",
+  phone: 9792733752,
+  location: { 
+    streetAddress: "710 Encinas Pl. College Station, TX 77845", 
+    coordinates: { lat: 30.578982, lng: -96.310234 } 
+  },
+  lawn: { lotSize: 0.26, turfType: "Bermuda", lastMowed: 1501385439 }
+});
+clientList.push({
+  name: "Ross Fairbanks",
+  phone: 9792839103,
+  location: { 
+    streetAddress: "3005 Durango St. College Station, TX 77845", 
+    coordinates: { lat: 30.578355, lng: -96.312721 } 
+  },
+  lawn: { lotSize: 0.30, turfType: "St. Augustine", lastMowed: 1501385439 }
+});
+clientList.push({
+  name: "Christopher Chen",
+  phone: 7132835909,
+  location: { 
+    streetAddress: "1008 Oakhaven Cir. College Station, TX 77840", 
+    coordinates: { lat: 30.635970, lng: -96.312009 } 
+  },
+  lawn: { lotSize: 0.28, turfType: "St. Augustine", lastMowed: 1501385439 }
+});
+clientList.push({
+  name: "Dustin Avery",
+  phone: 9792038738,
+  location: { 
+    streetAddress: "1110 Woodhaven Cir. College Station, TX 77840", 
+    coordinates: { lat: 30.634736, lng: -96.311362 } 
+  },
+  lawn: { lotSize: 0.45, turfType: "St. Augustine", lastMowed: 1501385439 }
+});
+clientList.push({
+  name: "Fabian Ros",
+  phone: 7132849083,
+  location: { 
+    streetAddress: "1406 Post Oak Cir. College Station, TX 77840", 
+    coordinates: { lat: 30.635586, lng: -96.313606 } 
+  },
+  lawn: { lotSize: 0.33, turfType: "Zoysia", lastMowed: 1501385439 }
+});
+clientList.push({
+  name: "Lauren Ramirez",
+  phone: 7138592038,
+  location: { 
+    streetAddress: "1603 Francis Dr. College Station, TX 77840", 
+    coordinates: { lat: 30.635306, lng: -96.313084 } 
+  },
+  lawn: { lotSize: 0.66, turfType: "St. Augustine", lastMowed: 1501385439 }
+});
+clientList.push({
+  name: "Joshua Martinez",
+  phone: 9364828102,
+  location: { 
+    streetAddress: "200 College View Dr. Bryan, TX 77801", 
+    coordinates: { lat: 30.633307, lng: -96.350066 } 
+  },
+  lawn: { lotSize: 0.25, turfType: "St. Augustine", lastMowed: 1501385439 }
+});
+clientList.push({
+  name: "Miguel Hernandez",
+  phone: 9798271038,
+  location: { 
+    streetAddress: "3808 Ridgewood St. Bryan, TX 77801", 
+    coordinates: { lat: 30.635153, lng: -96.347800 } 
+  },
+  lawn: { lotSize: 0.47, turfType: "St. Augustine", lastMowed: 1501385439 }
+});
+clientList.push({
+  name: "Lindsey Velasco",
+  phone: 9369182793,
+  location: { 
+    streetAddress: "306 Day Ave. Bryan, TX 77801", 
+    coordinates: { lat: 30.635465, lng: -96.350541 } 
+  },
+  lawn: { lotSize: 0.25, turfType: "Bermuda", lastMowed: 1501385439 }
+});
+clientList.push({
+  name: "Daniel Johnson",
+  phone: 7132638390,
+  location: { 
+    streetAddress: "306 Crescent Dr. Bryan, TX 77801", 
+    coordinates: { lat: 30.632217, lng: -96.347544 } 
+  },
+  lawn: { lotSize: 0.52, turfType: "St. Augustine", lastMowed: 1501385439 }
 });
 
-// populate each albums song list
-// for each album in clientList set album.songs to be sampleSongs for that album S3S2 TC
-clientList.forEach(function(album) {
-  album.songs = sampleSongs;
-});
-
-
-db.Album.remove({}, function(err, albums){
-
-  db.Album.create(clientList, function(err, albums){
+db.Client.remove({}, function(err, clients){
+  db.Client.create(clientList, function(err, clients){
     if (err) { return console.log('ERROR', err); }
-    console.log("all albums:", albums);
-    console.log("created", albums.length, "albums");
+    console.log("All clients:", clients);
+    console.log("Created", clients.length, "clients");
     process.exit();
   });
 });
