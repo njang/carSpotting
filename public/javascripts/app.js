8bea67ad92bf663b642a18b9f82f4222387417a4
njang <neo.w.jang@gmail.com>
2018-01-21 00:18:05 -0600

    Add icons to client cards

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,4 @@
+
 (() => {
   $.ajax({
     method: 'GET',
@@ -11,13 +12,19 @@
 })();
 
 const clientCard = (client) => {
-  let cardElement = $('<div>', {class: 'clientCard card bg-primary text-white col col-3'});
+  let cardElement = $('<div>', {class: 'clientCard card bg-dark text-white col col-3'});
+  cardElement.append($('<i>', {class: 'material-icons', text: 'person'}));
   cardElement.append($('<h4>', {text: client.name}));
-  // debugger;
+  cardElement.append($('<i>', {class: 'material-icons', text: 'home'}));
   cardElement.append($('<p>', {text: client.location.streetAddress}));
-  cardElement.append($('<p>', {text: client.phone}));
-  // let displayElement = $('<input>', {id: 'display', placeholder: 0, disabled: true})
-
-  // cardElement.attr('text', client.name);
+  cardElement.append($('<i>', {class: 'material-icons', text: 'phone'}));
+  cardElement.append($('<a>', {href: 'tel:' + client.phone, text: formatPhoneNumber(client.phone)}));
+  cardElement.append($('<p>', {text: 'Last mowed: ' + Date(client.lawn.lastMowed)}));
+  cardElement.append($('<i>', {class: 'material-icons', text: 'delete'}));
   $('#clients').append(cardElement);
+}
+
+const formatPhoneNumber = (phoneNumber) => {
+  let digits = phoneNumber.toString().split('');
+  return '(' + digits.slice(0, 3).join('') + ') ' + digits.slice(3, 6).join('') + '-' + digits.slice(-4).join('');
 }
