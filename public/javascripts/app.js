88fa42a6cff8917e75d01af0bdaea202b6304c7b
njang <neo.w.jang@gmail.com>
2018-01-20 23:29:05 -0600

    Stylize client cards

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,4 @@
 (() => {
-  console.log('/public/javascript/app.js is loaded');
   $.ajax({
     method: 'GET',
     url: '/api/clients',
@@ -12,8 +11,10 @@
 })();
 
 const clientCard = (client) => {
-  let cardElement = $('<div>', {class: 'clientCard'});
-  cardElement.append($('<h3>', {text: client.name}));
+  let cardElement = $('<div>', {class: 'clientCard card bg-primary text-white col col-3'});
+  cardElement.append($('<h4>', {text: client.name}));
+  // debugger;
+  cardElement.append($('<p>', {text: client.location.streetAddress}));
   cardElement.append($('<p>', {text: client.phone}));
   // let displayElement = $('<input>', {id: 'display', placeholder: 0, disabled: true})
 
