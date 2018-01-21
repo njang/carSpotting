9d57f7f8f2bc7fdc7c53ff2cd3e3eb55af570e5d
njang <neo.w.jang@gmail.com>
2018-01-20 23:20:11 -0600

    Display client information in card format

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,14 +1,22 @@
 (() => {
-	console.log('/public/javascript/app.js is loaded');
-	$.ajax({
+  console.log('/public/javascript/app.js is loaded');
+  $.ajax({
     method: 'GET',
     url: '/api/clients',
     success: (clients) => {
-    	console.log(clients);
-			clients.forEach((client) => {
-				console.log(client.name);
-			});
+      clients.forEach((client) => {
+    	clientCard(client);
+      });
     }
   });
-
 })();
+
+const clientCard = (client) => {
+  let cardElement = $('<div>', {class: 'clientCard'});
+  cardElement.append($('<h3>', {text: client.name}));
+  cardElement.append($('<p>', {text: client.phone}));
+  // let displayElement = $('<input>', {id: 'display', placeholder: 0, disabled: true})
+
+  // cardElement.attr('text', client.name);
+  $('#clients').append(cardElement);
+}
