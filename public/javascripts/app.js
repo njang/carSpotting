270688726d1bf60b29b5d9fb2fb444cc62042c5f
njang <neo.w.jang@gmail.com>
2018-01-22 10:18:58 -0600

    Add responsive styling to client cards in mobile viewing

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,7 +5,8 @@
     url: '/api/clients',
     success: (clients) => {
       clients.forEach((client) => {
-        $('#clients').append(clientCard(client));
+        // $('#clients').append(clientCard(client));
+        clientCard(client);
       });
     }
   });
@@ -29,24 +30,24 @@ function init() {
 
 
 const clientCard = (client) => {
-  let cardElement = $('<div>', {class: 'clientCard card bg-dark text-white col col-4', 'data-client-id': client._id});
+  let cardElement = $('<div>', {class: 'clientCard card bg-dark text-white col-sm-2 col-md-3', 'data-client-id': client._id});
 
-  let divElement = $('<div>' , {class: 'card-row row'});
-  divElement.append($('<i>', {class: 'col col-2 material-icons', text: 'person'}));
+  let divElement = $('<div>' , {class: 'card-row-user row'});
+  divElement.append($('<i>', {class: 'col col-2 material-icons text-success', text: 'person'}));
   divElement.append($('<h4>', {class: 'col col-10', text: client.name}));
   cardElement.append(divElement);
 
-  divElement = $('<div>' , {class: 'card-row row'});
+  divElement = $('<div>' , {class: 'card-row-address row'});
   divElement.append($('<i>', {class: 'col col-2 material-icons', text: 'home'}));
-  divElement.append($('<p>', {class: 'col col-10', text: client.location.streetAddress}));
+  divElement.append($('<p>', {class: 'col col-10', html: formatAddress(client.location.streetAddress)}));
   cardElement.append(divElement);
 
-  divElement = $('<div>' , {class: 'card-row row'});
+  divElement = $('<div>' , {class: 'card-row-phone row'});
   divElement.append($('<i>', {class: 'col col-2 material-icons', text: 'phone'}));
   divElement.append($('<a>', {class: 'col col-10', href: 'tel:' + client.phone, text: formatPhoneNumber(client.phone)}));
   cardElement.append(divElement);
 
-  divElement = $('<div>' , {class: 'card-row row'});
+  divElement = $('<div>' , {class: 'card-row-lawn row'});
   divElement.append($('<i>', {class: 'col col-2 material-icons', text: 'schedule'}));
   divElement.append($('<p>', {class: 'col col-10', text: 'Last mowed: ' + howLongSince(client.lawn.lastMowed)}));
   cardElement.append(divElement);   
@@ -54,9 +55,9 @@ const clientCard = (client) => {
   // divElement = $('<div>' , {class: 'card-row row'});
   // cardElement.append($('<input>', {type: 'button', class: 'btn btn-remove-client btn-danger', value: 'remove', onclick: 'handleDeleteClientClick()'}));
   // divElement.append($('<button>', {class: 'btn btn-remove-client btn-danger', text: 'remove'}));
-  // $('#clients').append(cardElement);
   // cardElement.append(divElement);   
-  return cardElement;
+  // return cardElement;
+  $('#clients').append(cardElement);
 }
 
 const formatPhoneNumber = (phoneNumber) => {
@@ -64,14 +65,20 @@ const formatPhoneNumber = (phoneNumber) => {
   return '(' + digits.slice(0, 3).join('') + ') ' + digits.slice(3, 6).join('') + '-' + digits.slice(-4).join('');
 }
 
+const formatAddress = (address) => {
+  let addressLines = address.split('.');
+  return addressLines[0] + '<br>' + addressLines[1];
+}
+
+// Calculates how long ago the time of event was, and returns it in unit of days.
 const howLongSince = (timeOfEvent) => {
     // debugger;
   let dt = Math.floor((new Date() - timeOfEvent * 1000) / 1000 / 60 / 60 / 24);
   let message = "";
-  if (dt <= 1) {
-    message = 'Yesterday';
-  } else if (dt > 1) {
+  if (dt > 1) {
     message = dt + ' days ago';
+  } else {
+    message = 'Yesterday';
   }
   return message;
 }
