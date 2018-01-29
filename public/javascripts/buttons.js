c934ad7eba530c89be9e22b6c904c47405fbe277
njang <neo.w.jang@gmail.com>
2018-01-29 01:37:46 -0600

    Revise remove client confirmation modal

--- public/javascripts/buttons.js
+++ public/javascripts/buttons.js
@@ -9,17 +9,9 @@
 // Partial entry of address uses Google Maps Geocode API to parse to formatted street address and GPS coordinates
 $(document).on('click', '.saveNewClient', (e) => {
   e.preventDefault();
-
-  let newClientInput = e.target.parentElement.parentElement.parentElement;
-
-  // Output the captured information on the New Client Form  
-  console.log('Saving new client');
-  console.log('Name: ' + newClientInput.name.value);
-  console.log('Address: ' + newClientInput.address.value);
-  console.log('Phone: ' + formatPhoneNumber(newClientInput.phone.value));
-  console.log('Lawn: ' + newClientInput.turfType.value);
-  console.log('Last mowed: ' + newClientInput.lastMowed.value);
-
+  // let newClientInput = e.target.parentElement.parentElement.parentElement;
+  let newClientInput = e.target.closest('form');
+  // debugger;
   // Add geocode converter
   const endpoint = 'https://maps.googleapis.com/maps/api/geocode/json?address='
   let url = endpoint + encodeURIComponent(newClientInput.address.value);
@@ -34,10 +26,10 @@ $(document).on('click', '.saveNewClient', (e) => {
       let message = '';
       switch (responseData.status) {
         case 'OK':
-          geocodeResult.address = responseData.results[0].formatted_address;
-          geocodeResult.lat = responseData.results[0].geometry.location.lat;
-          geocodeResult.lng = responseData.results[0].geometry.location.lng;
-          console.log(geocodeResult);
+          // geocodeResult.address = responseData.results[0].formatted_address;
+          // geocodeResult.lat = responseData.results[0].geometry.location.lat;
+          // geocodeResult.lng = responseData.results[0].geometry.location.lng;
+          // console.log(geocodeResult);
           break;
         case 'ZERO_RESULTS':
           message = 'No coordinates found';
@@ -59,15 +51,16 @@ $(document).on('click', '.saveNewClient', (e) => {
       },
       phone: newClientInput.phone.value,
       lawn: {
+        lotSize: newClientInput.lotSize.value,
         turfType: newClientInput.turfType.value,
         lastMowed: newClientInput.lastMowed.value
       }
     },
     success: () => {
-    	console.log('new client success!')
+    	console.log('New client added successfully.')
     },
     error: () => {
-  		console.log('new client error!')
+  		console.log('Error: new client was not added.')
     }
   });
 
@@ -102,7 +95,7 @@ $(document).on('click', '.btn-remove-client', (e) => {
   e.preventDefault();
   let targetClientId = e.target.closest('.clientCard').dataset.clientId;
   // Populate the input fields with current values
-  $('.removeClientConfirm').val(e.target.closest('.clientCard').childNodes[0].childNodes[1].textContent);
+  $('.removeClientConfirm').text(e.target.closest('.clientCard').childNodes[0].childNodes[1].textContent);
 
   $(document).on('click', '.btn-remove-client-confirm', (e) => {
     e.preventDefault();
