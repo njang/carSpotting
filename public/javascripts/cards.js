90ce9b4bfa64529dbd66772f57730199ae791d74
njang <neo.w.jang@gmail.com>
2018-01-28 21:02:05 -0600

    Revise howLongSince() to accommodate string form entries

--- public/javascripts/cards.js
+++ public/javascripts/cards.js
@@ -82,3 +82,16 @@ const renderClientCards = () => {
     }
   });
 }
+
+// Calculates how long ago the time of event was, and returns it in unit of days
+const howLongSince = (timeOfEvent) => {
+  // let dt = Math.floor((new Date() - timeOfEvent * 1000) / 1000 / 60 / 60 / 24);
+  let dt = Math.floor(((new Date() - new Date(timeOfEvent)) / 1000 / 60 / 60 / 24));
+  let message = "";
+  if (dt > 1) {
+    message = dt + ' days ago';
+  } else {
+    message = 'Yesterday';
+  }
+  return message;
+}
